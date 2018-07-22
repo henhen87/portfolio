b51cf6c51304da4191e5b57a639e488ddf911a9b
henry <btnysci@yahoo.com>
2018-07-22 02:53:15 -0700

    good ball

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -27,6 +27,35 @@ class Home extends Component {
 
 	componentDidMount () {
 		let index = 0;
+		let canvas = document.getElementById("myCanvas");
+		let ctx = canvas.getContext("2d");
+		let ballRadius = 10;
+		let x = canvas.width / 2;
+		let y = canvas.height - 30;
+		let dx = 2;
+		let dy = -2;
+
+		function drawBall() {
+		    ctx.beginPath();
+		    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+		    ctx.fillStyle = "#000000";
+		    ctx.fill();
+		    ctx.closePath();
+		}
+		function draw() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			drawBall();
+
+			if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		        dx = -dx;
+		    }
+		    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+		        dy = -dy;
+		    }
+			x += dx;
+			y += dy;
+		}
+		setInterval(draw, 10);
 
 		setInterval(() => {
 			if (this.state.pause === false) {
@@ -51,6 +80,15 @@ class Home extends Component {
 		});
 	}
 
+	split = (e) => {
+		console.log('SPLIT')
+		this.setState({
+			split: !this.state.split
+		});
+
+		this.props.history.push('about');
+	}
+
 	render () {
 		console.log('PARAMS', this.props.match.params);
 		// let MySection = components[this.state.section];
@@ -59,16 +97,9 @@ class Home extends Component {
 		return (
 			<section id="main">
 				<section className={`menu ${this.state.split ? 'split' : ''}`}>
-					<div className="title">
-						<h1>Core Skills</h1>
-					</div>
 					<div className="scene">
 						<div className={`cube show-${this.state.currentSide}`}>
-							<div onClick={e => {
-								e.preventDefault();
-								this.props.history.push('/best-website/about');
-								// this.setState({ section: 'about', split: true });
-							}} className="cube__face cube__face--front">About Me</div>
+							<div onClick={this.split} value="about" className="cube__face cube__face--front">About Me</div>
 							<div className="cube__face cube__face--back">Skills</div>
 							<div className="cube__face cube__face--right">Resume</div>
 							<div className="cube__face cube__face--left">Experience</div>
@@ -76,6 +107,7 @@ class Home extends Component {
 							<div className="cube__face cube__face--bottom">Contact Me</div>
 						</div>
 					</div>
+					<canvas id="myCanvas" width="200" height="200"></canvas>
 				</section>
 				<section className={`description ${this.state.split ? 'split' : ''}`}>
 					{/*
@@ -83,6 +115,14 @@ class Home extends Component {
 					*/}
 					<Route path='/best-website/:section' component={MySection} />
 				</section>
+				<iframe 
+					id="bach"
+					width="1" 
+					height="1" 
+					src="https://www.youtube.com/embed/Lp39Z_76_n0?playlist=Lp39Z_76_n0&loop=1&autoplay=1" 
+					frameBorder="0" 
+					allow="autoplay; encrypted-media; loop" 
+				></iframe>
 			</section>
 		);
 	}
