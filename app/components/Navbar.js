227e8e51131377b4cb36256193b8ed58284e9770
henry <btnysci@yahoo.com>
2018-07-29 19:12:10 -0700

    navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
-	showNav () {
-		let navbar = document.querySelector('#navb');
-		let arrowDown = document.querySelector('.arrowDown');
+	state = {
+		showNav: false
+	}
 
-		navbar.classList.add('showNav');
-		arrowDown.classList.add('hideArrowDown');
+	showNav = () => {
+		// let navbar = document.querySelector('#navb');
+		// let arrowDown = document.querySelector('.arrowDown');
+
+		// navbar.classList.add('showNav');
+		// arrowDown.classList.add('hideArrowDown');
+
+		this.setState({ showNav: !this.state.showNav });
 	}
 
 	render () {
 		return [
-			<div className="arrowDown" onClick={this.showNav}>&#x02913;</div>,
-			<div id="navb" className="container-fluid">
+			<div 
+				className={`arrow ${this.state.showNav === false ? "arrowDown" : "arrowUp"}`} 
+				onClick={this.showNav}
+			>{this.state.showNav === false ? <span>&#x02913;</span> : <span>&#x02912;</span>}</div>,
+			<div id="navb" className={`container-fluid ${this.state.showNav === true ? 'showNav' : ''}`}>
 				<div className="row">
 					<div className="col-sm-2">
 						<Link to="">About Me</Link>
