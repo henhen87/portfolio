dfcef880c99af8af65c7c63b046d3dc170e1155d
henry <btnysci@yahoo.com>
2019-09-04 18:15:23 -0700

    dggsdg

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -12,143 +12,70 @@ import MusicPlayer from 'components/MusicPlayer';
 // };
 
 class Home extends Component {
-	// constructor (props) {
-	// 	super(props);
-
-	// 	this.state = {
-	// 		checked: null,
-	// 		sides: ['front', 'right', 'left', 'bottom', 'top', 'back'],
-	// 		pause: false,
-	// 		currentSide: '',
-	// 		split: false
-	// 	}
 	state = {
-		max: 0,
-		// mute: 0,
-		bgIndex: 1
+		pw: null,
+		pwExtra: null
 	}
-	// maxInterval
 
-	// setMax = () => {
-	// 	this.setState({
-	// 		max: this.state.max <= 35 ? this.state.max + 1 : 0,
-	// 	});
-	// }
+	fantasy = (e) => {
+		e.preventDefault();
 
-	// interval = () => {
-	// 	console.log('INSIDE SET INTERVAL')
-	// 	this.setState({
-	// 		switchSlides: !this.state.switchSlides,
-	// 	});
-	// 	// this.state.switchSlides ? this.setState({mute: 1}) : null
-	// }
+		this.setState({
+			pw: e.target.value
+		});
+	}
 
-	componentDidMount = () => {
-		// console.log('PLAYER', player.getPlaylistIndex())
-		// console.log('INSIDE DID MOUNTTT')
-		// this.maxInterval = setInterval(this.setMax, 1000);
-		// console.log('MAX', this.maxInterval)
-		// setInterval(this.setMax, 1000);
-
-		setInterval(() => {
+	readFantasy = () => {
+		if (Number(this.state.pw) === 2016) {
 			this.setState({
-				bgIndex: this.state.bgIndex === 7 ? 1 : this.state.bgIndex + 1
+				pwExtra: 2016
 			})
-		}, 20000)
+		}
 	}
-
-	// 	this.state.sides.forEach(e => {
-	// 		this[e] = React.createRef();
-	// 	});
-	// }
-
-	// componentDidMount () {
-	// 	//ball
-	// 	let index = 0;
-	// 	let canvas = document.getElementById("myCanvas");
-	// 	let ctx = canvas.getContext("2d");
-	// 	let ballRadius = 10;
-	// 	let x = canvas.width / 2;
-	// 	let y = canvas.height - 30;
-	// 	let dx = 2;
-	// 	let dy = -2;
-
-	// 	function drawBall() {
-	// 	    ctx.beginPath();
-	// 	    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	// 	    ctx.fillStyle = "#000000";
-	// 	    ctx.fill();
-	// 	    ctx.closePath();
-	// 	}
-	// 	function draw() {
-	// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// 		drawBall();
-
-	// 		if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-	// 	        dx = -dx;
-	// 	    }
-	// 	    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-	// 	        dy = -dy;
-	// 	    }
-	// 		x += dx;
-	// 		y += dy;
-	// 	}
-	// 	setInterval(draw, 10);
-
-	// 	setInterval(() => {
-	// 		if (this.state.pause === false) {
-	// 			this.setState({ 
-	// 				currentSide: this.state.sides[index]
-	// 			});
-	// 			index++;
-	// 		}
-	// 		if (index === 6) {
-	// 			index = 0;
-	// 		}
-	// 	}, 2000);
-	// }
-
-	// split = (e, i) => {
-	// 	if (i) {
-	// 		console.log('iiii', i)
-	// 		this.setState({ 
-	// 			currentSide: this.state.sides[i]
-	// 		});
-	// 	}
-
-	// 	this.setState({
-	// 		split: !this.state.split,
-	// 		pause: !this.state.pause,
-	// 		currentSection: e.target.getAttribute('data-value')
-	// 	});
-	// 	if (e.target.getAttribute('data-value') !== this.props.match.params.section && this.state.split === false) {
-	// 		this.props.history.push(e.target.getAttribute('data-value'));
-	// 	}
-
-	// }
 
 	render () {
-		// let MySection = components[this.props.match.params.section];
-		const background = {
-			background: `url(../img/space${this.state.bgIndex}.jpg)`,
-			backgroundRepeat: 'no-repeat',
-			backgroundSize: 'contain'
-		}
-		const font = {
-			color: 'pink',
-			fontSize: '25px',
-			textDecoration: 'underline'
-		}
-		const heart = '<3'
+
+
 		return (
-			<section id="main" style={background}>
-				<MusicPlayer />
-				<a href="https://youtu.be/gbvSQRHUm6c" style={font} target="_blank" onClick={() => player.pauseVideo()}>Forever</a>
+			<section id="main" style={{padding: '20px'}}>
+				<input type="text"  style={{width: '200px'}} placeholder="secret pw = year we first met" onChange={this.fantasy}/>
 				<br/>
 				<br/>
-				<br/>
-				<br/>
-				<a href="https://youtu.be/sxuxJpWnicU" style={font} target="_blank" onClick={() => player.pauseVideo()}>lvu</a>
+				<button type="button" onClick={this.readFantasy}>Read Message</button>
+
+				{
+					this.state.pwExtra === 2016
+						? (
+							<p>
+								Dear Elena, <br/><br/>
+								If you are reading this I guess you know how crazy I am about you... <br/>
+								Elena... I have tried so hard to forget about you... and some days I feel like I
+								have almost achieved that, but in the end, no matter how hard I try, you always win.
+								I cannot simply forget about you. I don't know what else to do anymore... Either time 
+								will go on and I will continue to dream about you or maybe fate will one day have us meet
+								again somehow.
+
+								Elena... I am so crazy about you. I do not know why... I have never felt this way before
+								for anyone. There were others I liked in the past I wish I could have been with, but they 
+								would simply just go away and be nothing more. Elena... you on the other hand are something else.
+								I must simply just have you and that is all. I have no interest for anyone else. Elena... I LOVE YOU.
+
+								I am sorry for getting all weird like this but I do not care at this point. Life is short.
+
+								Elena... I want to share with you one of my wild fantasies I have about you and I am sorry for this.. but
+								if you do not mind please read ahead. Elena I trust you... I only want you to read this if you are comfortable and
+								okay with it.
+
+								Elena... I hope one day if we meet again, it will be a very hot, humid and sticky day. I day dreamed today
+								that if I saw you on such a sticky humid hot day, I would firmly grab you by the arm and push you against a wall. 
+								I would not mind the nastiness... I would want every little bit of you. I want to firmly grasp both your hands and clasp 
+								our fingers together...
+							 	I would go on but I think I will stop here...
+
+								I love you Elena and I want you to know no matter what happens, I will always be thinking of you. 
+							</p>
+						) : null
+				}
 			</section>
 		);
 	}
